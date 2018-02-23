1faf2c09c42f4958d3243adf2c345896fb30623d
Suz <soutsada@comcast.net>
2018-02-22 18:49:32 -0600

    Website Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
-import { AppComponent } from './app.component';
+import { SuzComponent } from './suzwebsite/suz.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    SuzComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [SuzComponent]
 })
 export class AppModule { }
